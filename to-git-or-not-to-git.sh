7822ed0204b2b4e7cb980684dee2221ae3ea0acf
7usain7 <h.jameel2004@gmail.com>
2025-01-12 17:53:09 +0300

    Hope it works

--- to-git-or-not-to-git.sh
+++ to-git-or-not-to-git.sh
@@ -1,1 +1,4 @@
-echo
+curl -s https://learn.reboot01.com/assets/superhero/all.json | jq '.name | select(.id == 170)'
+curl -s https://learn.reboot01.com/assets/superhero/all.json | jq '.powerstats.power | select(.id == 170)'
+curl -s https://learn.reboot01.com/assets/superhero/all.json | jq '.appearance.gender | select(.id == 170)'
+
