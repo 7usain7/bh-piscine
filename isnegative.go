7a005d335f5a537740392d2e3181ac702234152c
7usain7 <h.jameel2004@gmail.com>
2025-01-14 14:01:02 +0300

    ok

--- isnegative.go
+++ isnegative.go
@@ -1,12 +1,11 @@
 package main
 
-import "github.com/01-edu"
+import "github.com/01-edu/z01"
 
 func IsNegative(nb int) {
-
 	if nb < 0 {
 		z01.PrintRune('T')
-	} else{
+	} else {
 		z01.PrintRune('F')
 	}
 }
