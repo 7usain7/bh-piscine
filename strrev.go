e29e213ff9e6a224ce0258a5c7c7f2fa929e1524
7usain7 <h.jameel2004@gmail.com>
2025-01-14 16:40:19 +0300

    it works now

--- strrev.go
+++ strrev.go
@@ -1,9 +1,10 @@
 package piscine
 
-func StrRev(s string) {
+func StrRev(s string) string {
 	rev := ""
 	arr := []rune(s)
 	for i := len(arr) - 1; i >= 0; i-- {
 		rev += string(arr[i])
 	}
+	return rev
 }
