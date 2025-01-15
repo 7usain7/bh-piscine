b386bbd68a07a492a27e05279f0b25e9744fe746
7usain7 <h.jameel2004@gmail.com>
2025-01-15 18:34:45 +0000

    Update 'atoi.go'

--- atoi.go
+++ atoi.go
@@ -11,21 +11,17 @@ func Atoi(s string) int {
 		if !(ascii > '9' || ascii < '0') {
 			number := ascii - '0'
 			integer = integer*10 + number
-		} else if i == 0 {
-			if arr[i] == '+' || arr[i] == '-' {
-				continue
-			}
+		} else if i == 0 && (arr[i] == '+' || arr[i] == '-') {
+			continue
 		} else {
 			integer = 0
 			break
 		}
 	}
-	if len(arr) == 1 || len(arr) == 2 {
+	if len(arr) == 1 {
 		return integer
 	}
-	if arr[1] == '-' || arr[1] == '+' {
-		return 0
-	} else if arr[0] == '-' {
+	if arr[0] == '-' {
 		return integer * -1
 	} else {
 		return integer
