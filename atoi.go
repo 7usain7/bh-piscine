1704d3c07c7cd89ce7ed813d986b11ff6a6e7530
7usain7 <h.jameel2004@gmail.com>
2025-01-15 10:13:26 +0000

    Update 'atoi.go'

--- atoi.go
+++ atoi.go
@@ -3,6 +3,9 @@ package piscine
 func Atoi(s string) int {
 	integer := 0
 	arr := []rune(s)
+	if len(arr) == 0 {
+		return 0
+	}
 	for i := 0; i < len(s); i++ {
 		ascii := int(arr[i])
 		if !(ascii > '9' || ascii < '0') {
@@ -14,6 +17,9 @@ func Atoi(s string) int {
 			integer = 0
 			break
 		}
+	}
+	if len(arr) == 2 {
+		return integer
 	}
 	if arr[1] == '-' || arr[1] == '+' {
 		return 0
