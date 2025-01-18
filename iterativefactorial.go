9f6081aa044cff6746ca51f55da9756c7ef39856
7usain7 <h.jameel2004@gmail.com>
2025-01-18 14:55:24 +0300

    iter

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -1,9 +1,16 @@
 package piscine
 
 func IterativeFactorial(nb int) int {
-	result := 1
-	for i := 1; i <= nb; i++ {
-		result *= i
+	if nb > 0 {
+		result := 1
+		for i := 1; i <= nb; i++ {
+			result *= i
+			if result < 0 {
+				return 0
+			}
+		}
+		return result
+	} else {
+		return 0
 	}
-	return result
 }
