7725dee23b5b63b4ecc1e812ea0d254a49f5671c
7usain7 <h.jameel2004@gmail.com>
2025-01-19 19:10:32 +0300

    ok

--- isprintable.go
+++ isprintable.go
@@ -4,7 +4,7 @@ func IsPrintable(s string) bool {
 	arr := []rune(s)
 	for i := 0; i < len(arr); i++ {
 		flag := false
-		for j := 0; j <= 32; j++ {
+		for j := 0; j < 32; j++ {
 			if arr[i] == rune(j) {
 				flag = true
 			}
