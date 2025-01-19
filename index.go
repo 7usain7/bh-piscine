5b5e2aae5a375738eb55281165750c9e4a83575f
7usain7 <h.jameel2004@gmail.com>
2025-01-19 18:42:55 +0300

    ok

--- index.go
+++ index.go
@@ -3,18 +3,15 @@ package piscine
 func Index(s string, toFind string) int {
 	sentence := []rune(s)
 	find := []rune(toFind)
-	flag := false
-	for i := 0; i < len(sentence); i++ {
-		if sentence[i] == find[0] {
-			flag = true
-			for j := 1; j < len(find); j++ {
-				flag = false
-				if sentence[i+j] == find[j] {
-					flag = true
-				}
+	for i := 0; i <= len(sentence)-len(find); i++ {
+		count := 0
+		for j := 0; j < len(find); j++ {
+			if sentence[i+j] != find[j] {
+				break
 			}
+			count++
 		}
-		if flag {
+		if count == len(find) {
 			return i
 		}
 	}
