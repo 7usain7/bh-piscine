113b03e01d4870b5a236c13ecf79aa54710c945e
7usain7 <h.jameel2004@gmail.com>
2025-01-23 03:02:59 +0300

    late night coding

---
+++ nbrconvertalpha/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"os"
+
+	"github.com/01-edu/z01"
+)
+
+func main() {
+	arr := os.Args
+	isUpper := false
+	if arr[1] == "--upper" {
+		isUpper = true
+	}
+	for i := 1; i < len(arr); i++ {
+		if i == 1 && isUpper {
+			continue
+		}
+		integer := 0
+		for j := 0; j < len(arr[i]); j++ {
+			if arr[i][j] < '0' || arr[i][j] > '9' {
+				z01.PrintRune(' ')
+				integer = 0
+				break
+			}
+			number := int(rune(arr[i][j]) - '0')
+			integer = integer*10 + number
+		}
+		if (integer > 0 && integer <= 26) && !isUpper {
+			z01.PrintRune(rune('a' + integer - 1))
+		} else if (integer > 0 && integer <= 26) && isUpper {
+			z01.PrintRune(rune('A' + integer - 1))
+		} else {
+			z01.PrintRune(' ')
+		}
+	}
+}
