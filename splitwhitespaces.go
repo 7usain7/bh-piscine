fff88db993ddae19c7344278a49755687af8330e
7usain7 <h.jameel2004@gmail.com>
2025-01-27 16:45:25 +0300

    ok

--- splitwhitespaces.go
+++ splitwhitespaces.go
@@ -3,15 +3,18 @@ package piscine
 func SplitWhiteSpaces(s string) []string {
 	arr := []string{}
 	word := ""
+	wordd := false
 	for i, val := range s {
 		if !(val == ' ' || val == '\t' || val == '\n') {
 			word += string(val)
+			wordd = true
 			if i == len(s)-1 {
 				arr = append(arr, word)
 			}
-		} else {
+		} else if (val == ' ' || val == '\t' || val == '\n') && wordd {
 			arr = append(arr, word)
 			word = ""
+			wordd = false
 		}
 	}
 	return arr
