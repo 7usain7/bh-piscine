2b11392d89f9ed1d27c4c15a19226d7c10524ad3
7usain7 <h.jameel2004@gmail.com>
2025-01-28 23:31:20 +0300

    ok

---
+++ doop/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func validOp(n string) bool {
+	return n == "+" || n == "-" || n == "/" || n == "*" || n == "%"
+}
+
+func main() {
+	args := os.Args[1:]
+
+	if len(args) != 3 || !validOp(args[1]) {
+		return
+	}
+	a, err := strconv.Atoi(args[0])
+	b, err1 := strconv.Atoi(args[2])
+	if err != nil || err1 != nil {
+		return
+	}
+	if (a > 0 && a+b < 0) || (a < 0 && a+b > 0) {
+		return
+	}
+	if args[1] == "+" {
+		fmt.Print(a + b)
+	} else if args[1] == "-" {
+		fmt.Print(a - b)
+	} else if args[1] == "*" {
+		fmt.Print(a * b)
+	} else if args[1] == "/" {
+		if b != 0 {
+			fmt.Print(a / b)
+		} else {
+			fmt.Print("No division by 0")
+		}
+	} else if args[1] == "%" {
+		if b != 0 {
+			fmt.Print(a % b)
+		} else {
+			fmt.Print("No modulo by 0")
+		}
+	}
+}
