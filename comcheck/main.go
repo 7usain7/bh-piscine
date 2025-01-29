c130b8bb106cad15c71e72fd37e08a0d9d056b5e
7usain7 <h.jameel2004@gmail.com>
2025-01-29 22:39:23 +0300

    ok

---
+++ comcheck/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	args := os.Args[1:]
+	for _, val := range args {
+		if val == "galaxy 01" || val == "galaxy" || val == "01" {
+			fmt.Println("Alert!!!")
+		}
+	}
+}
