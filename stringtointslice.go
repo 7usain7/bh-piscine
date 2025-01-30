3ed39111250d8bb06b6aae37f8bf444f0aec230f
7usain7 <h.jameel2004@gmail.com>
2025-01-30 09:19:55 +0300

    ok

--- stringtointslice.go
+++ stringtointslice.go
@@ -1,8 +1,9 @@
 package piscine
 
 func StringToIntSlice(str string) []int {
-	arr := make([]int, len(str))
-	for i, val := range str {
+	runes := []rune(str)
+	arr := make([]int, len(runes))
+	for i, val := range runes {
 		arr[i] = int(val)
 	}
 	return arr
