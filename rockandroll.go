3242e12fa7da26a61b4ff20ded3d56f1561d0a48
7usain7 <h.jameel2004@gmail.com>
2025-01-30 09:58:21 +0300

    ok

---
+++ rockandroll.go
@@ -0,0 +1,14 @@
+package piscine
+
+func RockAndRoll(n int) string {
+	if n < 0 {
+		return "error: number is negative\n"
+	}
+	if n%2 == 0 {
+		return "rock\n"
+	} else if n%3 == 0 {
+		return "roll\n"
+	} else {
+		return "error: non divisible\n"
+	}
+}
