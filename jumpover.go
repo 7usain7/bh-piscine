42464c7c72b480a5ad4c1b36f80d3dd8ac6f8df3
7usain7 <h.jameel2004@gmail.com>
2025-01-30 09:08:52 +0300

    ok

--- jumpover.go
+++ jumpover.go
@@ -2,11 +2,11 @@ package piscine
 
 func JumpOver(str string) string {
 	if len(str) < 3 {
-		return "ok"
+		return "\n"
 	}
 	result := ""
 	for i := 2; i < len(str); i += 3 {
 		result += string(str[i])
 	}
-	return result
+	return result + "\n"
 }
