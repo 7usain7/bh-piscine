206c9f5558b3c87243bb061d078198b0bdcd83d4
7usain7 <h.jameel2004@gmail.com>
2025-01-30 05:23:26 +0000

    Update 'dealapackofcards.go'

--- dealapackofcards.go
+++ dealapackofcards.go
@@ -9,4 +9,4 @@ func DealAPackOfCards(deck []int) {
 	fmt.Printf("Player 2: %d, %d, %d\n", deck[3], deck[4], deck[5])
 	fmt.Printf("Player 3: %d, %d, %d\n", deck[6], deck[7], deck[8])
 	fmt.Printf("Player 4: %d, %d, %d\n", deck[9], deck[10], deck[11])
-}
+} //hi
